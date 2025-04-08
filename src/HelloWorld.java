c251bbeafb080689307bd46be48046f2bfd56db4
Emre <farkiforum@gmail.com>
2025-04-08 21:30:27 +0300

    set up2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,9 @@
 public class HelloWorld {
 
+    public static void main(String[] args) {
+
+        System.out.println("Hello World ! This project was created to learn about remote !");
+
+    }
+
 }
